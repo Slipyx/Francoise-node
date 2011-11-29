6625726134f841229ca741f12fc2f28dc28d0eea
Josh Koch <jdk1337@gmail.com>
2011-11-29 17:14:43 -0500

    Web server.

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ function Parse()
 }
 
 // Hello world web server
-/*var http = require('http');
+var http = require('http');
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('hello, world\n');
-}).listen(8124);*/
+}).listen(8124);

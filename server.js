05c7baa2e467cd81b220ebffba6a6af83ac9ed07
Josh Koch <jdk1337@gmail.com>
2011-12-01 02:48:07 -0500

    Conforms to JSLint now.

--- server.js
+++ server.js
@@ -16,86 +16,87 @@ var client = new irc.Client('irc.freenode.net', 'Francoise', {
     stripColors: false
 });
 
-client.addListener('message', function(nick, to, message) {
+client.addListener('message', function (nick, to, message) {
+    'use strict';
     //console.log(nick + ' => ' + to + ': ' + message);
-    if(message == '.stats') {
+    if (message === '.stats') {
         client.say(to, 'Uptime: ' + require('./uptime.js').format(process.uptime()) + ' | Memory: ' +
             process.memoryUsage().rss / 1024 / 1024 + ' MiB.');
-    }
-    else if(message == '.os') {
+    } else if (message === '.os') {
         var os = require('os');
         client.say(to, 'Uptime: ' + require('./uptime.js').format(os.uptime()) + ' | Memory: ' +
             os.freemem() / 1024 / 1024 + ' MiB / ' + os.totalmem() / 1024 / 1024 +
             ' MiB.');
-    }
-    else if(message == '.version') {
+    } else if (message === '.version') {
         client.say(to, 'Node.js: ' + process.versions.node +
             ' | v8: ' + process.versions.v8 +
             ' | ares: ' + process.versions.ares +
             ' | ev: ' + process.versions.ev +
             ' | openssl: ' + process.versions.openssl);
-    }
-    else if(message == '.check') {
-        //CheckFeed();
-    }
+    } /*else if (message === '.check') {
+        checkFeed();
+    }*/
 });
 
 // RSS PARSING
-var feed = {name: 'XKCD', url: 'http://xkcd.com/rss.xml'}
+var feed = {name: 'XKCD', url: 'http://xkcd.com/rss.xml'};
 var rssxml = '';
-function CheckFeed()
-{
+var parse;
+function checkFeed() {
+    'use strict';
     var request = require('request');
     request = request.defaults({jar: false});
-    request(feed.url, function(error, response, body) {
-        if(!error && response.statusCode == 200) {
+    request(feed.url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
             rssxml = body;
-            Parse();
+            parse();
         }
     });
 }
-function Parse()
-{
-    var sax = require('sax'), strict = true, parser = sax.parser(strict, {trim: true, normalize: true});
-    var inItem = false;
-    var inTitle = false;
-    var inDate = false;
-    var i = -1;
+function parse() {
+    'use strict';
+    var sax = require('sax'), strict = true,
+        parser = sax.parser(strict, {trim: true, normalize: true}),
+        inItem = false, inTitle = false, inDate = false,
+        i = -1, c = 0, ch = 0;
     feed.items = [];
-    parser.ontext = function(t) {
-        if(inItem && parser.tag) {
-            if(parser.tag.name == 'title') {
+    parser.ontext = function (t) {
+        if (inItem && parser.tag) {
+            if (parser.tag.name === 'title') {
                 feed.items[i].title = t;
-            }
-            else if(parser.tag.name == 'link' || parser.tag.name == 'id') {
+            } else if (parser.tag.name === 'link' || parser.tag.name === 'id') {
                 feed.items[i].link = t;
-            }
-            else if(parser.tag.name == 'pubDate' || parser.tag.name == 'dc:date' || parser.tag.name == 'updated') {
+            } else if (parser.tag.name === 'pubDate' || parser.tag.name === 'dc:date' || parser.tag.name === 'updated') {
                 feed.items[i].date = t;
             }
         }
-    }
-    parser.oncdata = function(t) {
-        if(inItem && inTitle) feed.items[i].title = t;
-        else if(inItem && inDate) feed.items[i].date = t;
-    }
-    parser.onopentag = function(node) {
-        if(node.name == 'item' || node.name == 'entry') {
-            inItem = true; ++i;
-            feed.items[i] = {};
+    };
+    parser.oncdata = function (t) {
+        if (inItem && inTitle) {
+            feed.items[i].title = t;
+        } else if (inItem && inDate) {
+            feed.items[i].date = t;
         }
-        else if(node.name == 'title') inTitle = true;
-        else if(node.name == 'pubDate' || node.name == 'dc:date') inDate = true;
-    }
-    parser.onclosetag = function(tag) {
-        if(tag == 'item' || tag == 'entry') inItem = false;
-        if(tag == 'title') inTitle = false;
-        if(tag == 'pubDate' || tag == 'dc:date') inDate = false;
-    }
+    };
+    parser.onopentag = function (node) {
+        if (node.name === 'item' || node.name === 'entry') {
+            inItem = true; i += 1;
+            feed.items[i] = {};
+        } else if (node.name === 'title') {
+            inTitle = true;
+        } else if (node.name === 'pubDate' || node.name === 'dc:date') {
+            inDate = true;
+        }
+    };
+    parser.onclosetag = function (tag) {
+        if (tag === 'item' || tag === 'entry') { inItem = false; }
+        if (tag === 'title') { inTitle = false; }
+        if (tag === 'pubDate' || tag === 'dc:date') { inDate = false; }
+    };
     parser.write(rssxml).close();
     //console.log(feed.items);
-    for(c = 0; c < feed.items.length; ++c) {
-        for(ch = 0; ch < client.opt.channels.length; ++ch) {
+    for (c = 0; c < feed.items.length; c += 1) {
+        for (ch = 0; ch < client.opt.channels.length; ch += 1) {
             client.say(client.opt.channels[ch], feed.name + ': ' + feed.items[c].title + ' <' + feed.items[c].link + '>');
         }
     }
@@ -103,7 +104,8 @@ function Parse()
 
 // Hello world web server
 var http = require('http');
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
+    'use strict';
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('hello, world\n');
 }).listen(80);
--- uptime.js
+++ uptime.js
@@ -1,25 +1,24 @@
-function hrmin(secs)
-{
-    var hr = Math.floor(secs / 60 / 60);
-    var min = Math.floor((secs - (hr * 60 * 60)) / 60);
+function hrmin(secs) {
+    'use strict';
+    var hr = Math.floor(secs / 60 / 60),
+        min = Math.floor((secs - (hr * 60 * 60)) / 60);
     return hr + ':' + ([1e15] + min).slice(-2);
 }
 
-exports.format = function(upsec)
-{
-    if(upsec < 60) return '0:00';
-    else if(upsec >= 60 && upsec < 86400) {
-        if(upsec > 86340) upsec = 86340;
+exports.format = function (upsec) {
+    'use strict';
+    if (upsec < 60) {
+        return '0:00';
+    } else if (upsec >= 60 && upsec < 86400) {
+        if (upsec > 86340) { upsec = 86340; }
         return hrmin(upsec);
-    }
-    else if(upsec >= 86400 && upsec < 172800) {
+    } else if (upsec >= 86400 && upsec < 172800) {
         return '1 day  ' + hrmin(upsec - 86400);
-    }
-    else if(upsec >= 172800) {
+    } else if (upsec >= 172800) {
         var days = Math.floor(upsec / 86400);
         upsec -= days * 86400;
         return days + ' days  ' + hrmin(upsec);
     }
 
     return 'WHAT' + upsec;
-}
+};
